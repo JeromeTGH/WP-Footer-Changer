95d044d3e62dec4b8fb846792c3c1f61306f7d20
JeromeTGH <jerome.tomski@gmx.fr>
2023-04-26 21:31:32 +0200

    Création du projet

---
+++ WP-Footer-Filter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WP-Footer-Filter
+ * @version 1.0.0
+ */
+/*
+Plugin Name: WP-Footer-Filter
+Plugin URI: https://github.com/JeromeTGH/WP-Footer-Filter
+Description: Filtre de pied de page, pour Wordpress
+Author: Jérôme TOMSKI
+Version: 1.0.0
+Author URI: https://github.com/JeromeTGH
+*/
